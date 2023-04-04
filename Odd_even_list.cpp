5e3b80004c041c989d858b33411718dd5b711e38
Kiran Samanta <119595085+Kiran1126@users.noreply.github.com>
2023-04-04 23:48:23 +0530

    Add files via upload

--- Odd_even_list.cpp
+++ Odd_even_list.cpp
@@ -1,17 +1,20 @@
 #include<stdio.h>
 int main()
 {
-	int i,n;
+	int i,n,flag=0;
 	printf("Enter your limit :");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++)
 	{
 		if(i%2==0)
 		{
+			flag = 1;
+			break;
 			printf("\n%d is a even number.",i);
 		}
 		else
 		{
+			flag = 0;
 			printf("\n%d is a odd number.",i);
 		}
 	}
